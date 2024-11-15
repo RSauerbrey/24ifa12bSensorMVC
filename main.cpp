8a5de265e4ae29bfabf5b4bf914c6a77de9f0adc
RSauerbrey <113389368+RSauerbrey@users.noreply.github.com>
2024-11-15 08:28:34 +0100

    Temperatursensor added

--- main.cpp
+++ main.cpp
@@ -1,12 +1,20 @@
 #include <iostream>
 #include "datagate.h"
 #include "feuchtesensor.h"
+#include "temperatursensor.h"
 
 using namespace std;
 
 int main()
 {
-    DataGate *dg = new FeuchteSensor;
+    DataGate *dG;
+    FeuchteSensor *f = new FeuchteSensor;
+    TemperaturSensor *t = new TemperaturSensor;
+    dG = f;
+    cout << dG->getSensorType() << endl;
+    dG = t;
+    cout << dG->getSensorType() << endl;
+
 
     return 0;
 }
